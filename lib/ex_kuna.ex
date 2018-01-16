6dea3fd976a24a1f0795336ba8ef7e86646387d5
kooler62 <kooler62@gmail.com>
2018-01-17 00:23:04 +0200

    refactor

--- lib/ex_kuna.ex
+++ lib/ex_kuna.ex
@@ -3,13 +3,11 @@ defmodule ExKuna do
   Documentation for KunaEx.
   """
 
-  @doc """
-  https://github.com/mfiyalka/kuna/blob/master/src/API/Kuna/ApiKuna.php
-  """
+
   @host "https://kuna.io/api/v2"
   @kuna "https://kuna.io"
-  @public_key "HC85Tcs2E926Dxu2PgFex6JKESmxZ5ymYDCm7fAx"
-  @secret_key "n7GkZpAnd0EQc5xERRJRqxgIx1xmqp9ONqxxkX12"
+  @public_key "dfsdkfksdjflkjsdflkdsjflkjd"
+  @secret_key "sdflkjdflkjsdlfkjldskjflkdsjf"
   @headers %{"Content-Type" => "application/json", "charset" => "utf-8"}
 
   def timestamp, do: get_request("#{@host}/timestamp")
@@ -20,31 +18,7 @@ defmodule ExKuna do
 
   def history(market \\ "btcuah"), do: get_request("#{@host}/trades?market=#{market}")
 
-
-  def get_request(url) do
-    case HTTPoison.get(url) do
-      {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
-        {:ok, body |> Poison.decode!}
-      {:error, %HTTPoison.Error{reason: reason}} ->
-        {:error, reason}
-    end
-  end
-
-  def post_request(url) do
-    HTTPoison.post(url, [], @headers)
-  end
-
-  def tonce(), do: "#{System.system_time(:second)}000"
-
-  def hash(string, key) do
-    :crypto.hmac(:sha256, key, string) |> Base.encode16 |> String.downcase
-  end
-
-  def sign(uri, params, method \\ "GET") do
-    string = "#{method}|#{uri}|#{params}"
-    :crypto.hmac(:sha256, @secret_key, string) |> Base.encode16 |> String.downcase
-  end
-  ############# USERS METHODS #############
+  # --- USER METHODS ---
 
   def user_info() do
     uri = "/api/v2/members/me"
@@ -76,20 +50,6 @@ defmodule ExKuna do
 
   end
 
-  # volume in btc
-  def order_create_test() do
-    side = "buy"
-    volume = "1"
-    market = "btcuah"
-    price = "1"
-
-    uri = "/api/v2/orders"
-    params="access_key=#{@public_key}&market=#{market}&price=#{price}&side=#{side}&tonce=#{tonce()}&volume=#{volume}"
-    url = "#{@kuna}#{uri}?#{params}&signature=#{sign(uri, params, "POST")}"
-    post_request(url)
-
-  end
-
   def order_delete(id) do
     uri = "/api/v2/order/delete"
     params = "access_key=#{@public_key}&id=#{id}&tonce=#{tonce()}"
@@ -97,5 +57,26 @@ defmodule ExKuna do
     post_request(url)
   end
 
+  # --- HELPER METHODS ---
+
+  def get_request(url) do
+    case HTTPoison.get(url) do
+      {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
+        {:ok, body |> Poison.decode!}
+      {:error, %HTTPoison.Error{reason: reason}} ->
+        {:error, reason}
+    end
+  end
+
+  def post_request(url) do
+    HTTPoison.post(url, [], @headers)
+  end
+
+  def tonce(), do: "#{System.system_time(:second)}000"
+
+  def sign(uri, params, method \\ "GET") do
+    string = "#{method}|#{uri}|#{params}"
+    :crypto.hmac(:sha256, @secret_key, string) |> Base.encode16 |> String.downcase
+  end
 
 end
